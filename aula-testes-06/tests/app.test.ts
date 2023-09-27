36ba8723811d48f7580de8dabbb4e758ce938b96
Luca-Panza <luca.panza.007@gmail.com>
2023-09-27 12:42:03 -0300

    feat: lesson 06

---
+++ aula-testes-06/tests/app.test.ts
@@ -0,0 +1,21 @@
+import supertest from "supertest";
+
+import app from "./../src/app";
+
+const api = supertest(app);
+
+describe("API test", () => {
+  it("should respond with status 200 and object structure", async () => {
+    const { status, body } = await api.get("/event");
+
+    const dateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
+
+    expect(status).toBe(200);
+    expect(body).toEqual({
+      id: expect.any(Number),
+      title: expect.any(String),
+      image: expect.any(String),
+      date: expect.stringMatching(dateRegex),
+    });
+  });
+});
